20c3884104aa1b220b579c7476f80ae83006f03a
dhekdani123 <143859936+dhekdani123@users.noreply.github.com>
2023-09-01 21:00:16 -0700

    Add files via upload

---
+++ belajar.py
@@ -0,0 +1,22 @@
+print("Hello")
+#variable tipe data
+print("Hello")
+x = 19 #integer
+y = 22
+z = 2
+q = 2
+t = z**q
+print ("Pangkat Hasil",t)
+#float
+b = 3.14
+#Boolean trueor false
+#Aritmatika
+c = x + y
+print ("Hasil Penjumlahan",c)
+c = y / z
+print ("Pembagian Hasil",c)
+c = x // z
+print ("Pembagian Hasil",c)
+c = x - y
+print ("Pengurangan Hasil",c) 
+nama = int (input("masukkan namaa"))
